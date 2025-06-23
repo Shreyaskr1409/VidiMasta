95b770facac9843d6574bf79da94ddff7d90b933
Shreyas Kumar <shreyaskr.0914@gmail.com>
2025-06-23 22:01:24 +0530

    Added authentication to certain handlers

--- gateway/routes/user.routes.go
+++ gateway/routes/user.routes.go
@@ -4,19 +4,26 @@ import (
 	"log"
 
 	"github.com/Shreyaskr1409/VidiMasta/gateway/handlers"
+	"github.com/Shreyaskr1409/VidiMasta/gateway/middlewares"
 	"github.com/gorilla/mux"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func HandleUserRoutes(router *mux.Router, l *log.Logger, db *pgxpool.Pool) {
-	userRouter := router.PathPrefix("/api/v1/users").Subrouter()
 	userHandler := handlers.NewUserHandler(l, db)
+	authMiddlewareHandler := middlewares.AuthMiddleware(db)
+
+	userRouter := router.PathPrefix("/api/v1/users").Subrouter()
 
 	userRouter.HandleFunc("/user", userHandler.GetUser).Methods("GET")
 	userRouter.HandleFunc("/{username}", userHandler.GetUserByUsername).Methods("GET")
 	userRouter.HandleFunc("/register", userHandler.Register).Methods("POST")
 	userRouter.HandleFunc("/login", userHandler.Login).Methods("POST")
 	userRouter.HandleFunc("/logout", userHandler.Logout).Methods("GET")
-	userRouter.HandleFunc("/update", userHandler.UpdateUser).Methods("PATCH")
-	userRouter.HandleFunc("/update-password", userHandler.UpdatePassword).Methods("PATCH")
+
+	authUserRouter := router.PathPrefix("/api/v1/users").Subrouter()
+	authUserRouter.Use(authMiddlewareHandler)
+
+	authUserRouter.HandleFunc("/update", userHandler.UpdateUser).Methods("PATCH")
+	authUserRouter.HandleFunc("/update-password", userHandler.UpdatePassword).Methods("PATCH")
 }
